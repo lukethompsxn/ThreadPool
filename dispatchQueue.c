8d548124e742ef690ca5ba3da64a6e46fa20a514
lukethompson <lukethompsxn@gmail.com>
2018-09-17 18:41:21 +1200

    task_create done

--- dispatchQueue.c
+++ dispatchQueue.c
@@ -1,33 +1,8 @@
-#include <dispatchQueue.h>
+#include "dispatchQueue.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-/*
-Creates a dispatch queue, probably setting up any associated threads and a linked list to be used by
-the added tasks. The queueType is either CONCURRENT or SERIAL.
-Returns: A pointer to the created dispatch queue.
-Example:
-dispatch_queue_t *queue;
-queue = dispatch_queue_create(CONCURRENT);
-*/
-dispatch_queue_t *dispatch_queue_create(queue_type_t queueType) 
-{
-
-}
-
-/*
-Destroys the dispatch queue queue. All allocated memory and resources such as semaphores are
-released and returned.
-Example:
-dispatch_queue_t *queue;
-…
-dispatch_queue_destroy(queue);
-*/
-void dispatch_queue_destroy(dispatch_queue_t *queue) 
-{
-
-}
+#include <string.h>
 
 /*
 Creates a task. work is the function to be called when the task is executed, param is a pointer to
@@ -36,92 +11,56 @@ parameter which the work function uses. If it is a single parameter it must eith
 something which can be cast to or from a pointer. The name is a string of up to 63 characters. This
 is useful for debugging purposes.
 Returns: A pointer to the created task.
-Example:
-void do_something(void *param) {
-long value = (long)param;
-printf(“The task was passed the value %ld.\n”, value);
-}
-task_t *task;
-task = task_create(do_something, (void *)42, “do_something”);
 */
-task_t *task_create(void (* work)(void *), void *param, char* name)
-{
-    
+task_t *task_create(void (*work)(void *), void *params, char* name) {
+    task_t *task = (task_t *)malloc(sizeof (task_t));
+    task->work = work;
+    task->params = params;
+    strcpy(task->name, name);
+    return task;
 }
 
 /*
 Destroys the task. Call this function as soon as a task has completed. All memory allocated to the
 task should be returned.
-Example:
-task_t *task;
-…
-task_destroy(task);
 */
-void task_destroy(task_t *task)
-{
-
-}
+void task_destroy(task_t *);
 
 /*
-Sends the task to the queue (which could be either CONCURRENT or SERIAL). This function does
-not return to the calling thread until the task has been completed.
-Example:
-dispatch_queue_t *queue;
-task_t *task;
-…
-dispatch_sync(queue, task);
+Creates a dispatch queue, probably setting up any associated threads and a linked list to be used by
+the added tasks. The queueType is either CONCURRENT or SERIAL.
+Returns: A pointer to the created dispatch queue.
 */
-void dispatch_sync(dispatch_queue_t *queue, task_t *task) 
-{
+dispatch_queue_t *dispatch_queue_create(queue_type_t);
 
-}
+/*
+Destroys the dispatch queue queue. All allocated memory and resources such as semaphores are
+released and returned.
+*/
+void dispatch_queue_destroy(dispatch_queue_t *);
 
 /*
 Sends the task to the queue (which could be either CONCURRENT or SERIAL). This function
 returns immediately, the task will be dispatched sometime in the future.
-Example:
-dispatch_queue_t *queue;
-task_t *task;
-…
-dispatch_async(queue, task);
 */
-void dispatch_async(dispatch_queue_t *queue, task_t *task)
-{
-
-}
+int dispatch_async(dispatch_queue_t *, task_t *);
 
 /*
-Waits (blocks) until all tasks on the queue have completed. If new tasks are added to the queue
-after this is called they are ignored.
-Example:
-dispatch_queue_t *queue;
-…
-dispatch_queue_wait(queue);
+Sends the task to the queue (which could be either CONCURRENT or SERIAL). This function does
+not return to the calling thread until the task has been completed.
 */
-void dispatch_queue_wait(dispatch_queue_t *queue)
-{
-
-}
+int dispatch_sync(dispatch_queue_t *, task_t *);
 
 /*
 Executes the work function number of times (in parallel if the queue is CONCURRENT). Each
 iteration of the work function is passed an integer from 0 to number-1. The dispatch_for
 function does not return until all iterations of the work function have completed.
-Example:
-void do_loop(long value) {
-printf(“The task was passed the value %ld.\n”, value);
-}
-dispatch_queue_t *queue;
-…
-dispatch_for(queue, 100, do_loop);
-This is sort of equivalent to:
-for (long i = 0; i < 100; i++)
-do_loop(i);
-Except the do_loop calls can be done in parallel.
 */
-void dispatch_for(dispatch_queue_t *queue, long number, void (*work) (long))
-{
-    
-}
+void dispatch_for(dispatch_queue_t *, long, void (*)(long));
 
+/*
+Waits (blocks) until all tasks on the queue have completed. If new tasks are added to the queue
+after this is called they are ignored.
+*/
+int dispatch_queue_wait(dispatch_queue_t *);
 
